1238ef243c6ab49f6f8fb4884e7964aad36faab3
kchuidev <121410967+kchuidev@users.noreply.github.com>
2022-12-30 21:07:51 +0800

    Add time on index.html

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,8 +1,13 @@
-var menu_icon = document.getElementById("menu_icon");
-var navigation_menu = document.getElementById("navigation_menu");
+/*
+  kchui.dev
+  menu.js
+*/
+
+const menu_icon = document.getElementById("menu_icon");
+const navigation_menu = document.getElementById("navigation_menu");
 menu_icon.addEventListener("click", menuOpen);
 
 function menuOpen() {
-	menu_icon.classList.toggle("menu-open");
-	navigation_menu.classList.toggle("menu-open");
+  menu_icon.classList.toggle("menu-open");
+  navigation_menu.classList.toggle("menu-open");
 }
