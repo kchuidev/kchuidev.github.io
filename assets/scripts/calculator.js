06280dc5ef63eef22d49bb9bfa3397bd54cfaf7b
kchuidev <121410967+kchuidev@users.noreply.github.com>
2023-01-02 21:57:09 +0800

    Add calculator

---
+++ assets/scripts/calculator.js
@@ -0,0 +1,84 @@
+/*
+  kchui.dev
+  calculator.js
+*/
+
+let formula;
+let number;
+let key;
+let integer;
+let formula_display = document.getElementById("formula");
+let number_display = document.getElementById("number");
+let buttons = document.getElementsByClassName("buttons");
+
+function initiate() {
+  formula = "";
+  number = "";
+  integer = true;
+  formula_display.innerHTML = "-";
+  number_display.innerHTML = 0;
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("mousedown", press);
+    buttons[i].addEventListener("click", input);
+  }
+}
+
+function display(value, type) {
+  switch (type) {
+    case ("formula"):
+      formula_display.innerHTML = formula;
+      return;
+    case ("number"):
+      if ( integer != false ) {
+        number_display.innerHTML = Number(value);
+      } else {
+        number_display.innerHTML = parseFloat(value);
+      }
+      return;
+  }
+}
+
+function press(event) {
+  event = event || window.event;
+  key = event.target;
+  key.classList.add("pressed");
+  setTimeout(()=>{key.classList.remove("pressed");}, 200);
+}
+
+function input(event) {
+  event = event || window.event;
+  key = event.target;
+  switch (true) {
+    case (key.hasAttribute("data-digit")):
+      formula += key.getAttribute("data-digit");
+      number += key.getAttribute("data-digit");
+      display(formula, "formula");
+      display(number, "number");
+      return;
+    case (key.hasAttribute("data-sign")):
+      switch ( key.getAttribute("data-sign") ) {
+        case ( "." ):
+          if ( integer != false ) {
+            integer = false;
+            formula += key.getAttribute("data-sign");
+            number += key.getAttribute("data-sign");
+            display(formula, "formula");
+            display(number, "number");
+            number_display.innerHTML += ".";
+          }
+          return;
+        case ( "equality" ):
+          number = eval(formula);
+          display(number, "number");
+          return;
+        default:
+          integer = true;
+          formula += key.getAttribute("data-sign");
+          number = "";
+          display(formula, "formula");
+          return;
+      }
+  }
+}
+
+initiate();
