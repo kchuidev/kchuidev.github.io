47fd7111d2b451ca99e4a97f9b4b7a0fc379ca46
kchuidev <121410967+kchuidev@users.noreply.github.com>
2023-01-14 15:53:14 +0800

    Refactor footer

---
+++ assets/scripts/footer.js
@@ -0,0 +1,9 @@
+/*
+  kchui.dev
+  footer.js
+*/
+
+const footer_content = "Created and maintained by kchui";
+const footer = document.getElementById("footer");
+
+footer.innerHTML = footer_content;
