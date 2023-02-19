de0e5345aac230d1854faa2580f6e30d6a754b25
kchuidev <121410967+kchuidev@users.noreply.github.com>
2023-02-19 18:03:34 +0800

    Fix Memories graph

--- assets/scripts/memories.js
+++ assets/scripts/memories.js
@@ -29,7 +29,9 @@ let index_colour = 0;
 
 function initiate() {
   event_recorded = {};
-  if ( events_stored != null && events_stored_loaded == false ) {
+  if ( events_stored.length > 0 && events_stored_loaded == false ) {
+    description.classList.add("hidden");
+    details.classList.remove("hidden");
     events_recorded = events_stored;
     events_recorded.forEach((e)=>{
       titles_stored.push(e.event);
