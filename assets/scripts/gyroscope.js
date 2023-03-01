318a842e70fd302f4a10ff3ab763e2df85f018e7
kchuidev <121410967+kchuidev@users.noreply.github.com>
2023-03-01 21:46:48 +0800

    Add Gyroscope

---
+++ assets/scripts/gyroscope.js
@@ -0,0 +1,28 @@
+/*
+  kchui.dev
+  gyroscope.js
+*/
+
+let playground = document.getElementById("playground");
+let gyroscope = new Gyroscope({ frequency: 60 });
+let reading_x = document.getElementById("reading_x");
+let reading_y = document.getElementById("reading_y");
+let reading_z = document.getElementById("reading_z");
+
+function initiate() {
+  let drawing = playground.getContext("2d");
+  drawing.fillStyle = "#FFFFFF";
+  drawing.fillRect(0, 0, 5, 5);
+  console.log(playground.width + " " + playground.height);
+}
+
+gyroscope.addEventListener("reading", (e) => {
+  reading_x.innerHTML = gyroscope.x;
+  reading_y.innerHTML = gyroscope.y;
+  reading_z.innerHTML = gyroscope.z;
+});
+
+window.addEventListener("load", ()=>{
+  initiate();
+  gyroscope.start();
+}, false);
