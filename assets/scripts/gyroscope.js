a77ce8462fed058dfc7df4e4099c755f30704ffd
kchuidev <121410967+kchuidev@users.noreply.github.com>
2023-03-03 10:23:29 +0800

    Fix Gyroscope

--- assets/scripts/gyroscope.js
+++ assets/scripts/gyroscope.js
@@ -23,9 +23,12 @@ function initiate() {
 }
 
 function startGame() {
+  reading_alpha.innerHTML = 0;
+  reading_beta.innerHTML = 0;
+  reading_gamma.innerHTML = 0;
   DeviceMotionEvent.requestPermission()
     .then(response => {
-      if (response === 'granted') {
+      if (response == 'granted') {
         window.addEventListener('deviceorientation', (event) => {
           reading_alpha.innerHTML = event.alpha;
           reading_beta.innerHTML = event.beta;
