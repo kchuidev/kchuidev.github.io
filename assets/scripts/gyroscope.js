f693262424207afa3e73d699e3fd576d0bede9bb
kchuidev <121410967+kchuidev@users.noreply.github.com>
2023-03-03 10:07:29 +0800

    Update Gyroscope

--- assets/scripts/gyroscope.js
+++ assets/scripts/gyroscope.js
@@ -5,24 +5,43 @@
 
 let playground = document.getElementById("playground");
 let gyroscope = new Gyroscope({ frequency: 60 });
-let reading_x = document.getElementById("reading_x");
-let reading_y = document.getElementById("reading_y");
-let reading_z = document.getElementById("reading_z");
+let button_start = document.getElementById("button_start");
+// let reading_x = document.getElementById("reading_x");
+// let reading_y = document.getElementById("reading_y");
+// let reading_z = document.getElementById("reading_z");
+let reading_alpha = document.getElementById("reading_alpha");
+let reading_beta = document.getElementById("reading_beta");
+let reading_gamma = document.getElementById("reading_gamma");
 
 function initiate() {
   let drawing = playground.getContext("2d");
   drawing.fillStyle = "#FFFFFF";
   drawing.fillRect(0, 0, 5, 5);
   console.log("width: " + playground.width + " & height:" + playground.height);
+
+  button_start.addEventListener("click", startGame, false);
 }
 
-gyroscope.addEventListener("reading", (e) => {
-  reading_x.innerHTML = gyroscope.x;
-  reading_y.innerHTML = gyroscope.y;
-  reading_z.innerHTML = gyroscope.z;
-});
+function startGame() {
+  // feature detect
+  if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+    DeviceOrientationEvent.requestPermission()
+      .then(permissionState => {
+        if (permissionState === 'granted') {
+          window.addEventListener('deviceorientation', (event) => {
+            reading_alpha.innerHTML = event.alpha;
+            reading_beta.innerHTML = event.beta;
+            reading_gamma.innerHTML = event.gamma;
+            return;
+          }, false);
+        }
+      })
+      .catch(console.error);
+  } else {
+    // handle regular non iOS 13+ devices
+  }
+}
 
 window.addEventListener("load", ()=>{
   initiate();
-  gyroscope.start();
 }, false);
