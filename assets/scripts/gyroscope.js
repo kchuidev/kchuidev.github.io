ff356f75626cce05c9fc23f2639bffbf855fb91b
kchuidev <121410967+kchuidev@users.noreply.github.com>
2023-03-03 14:42:26 +0800

    Update Gyroscope

--- assets/scripts/gyroscope.js
+++ assets/scripts/gyroscope.js
@@ -4,30 +4,38 @@
 */
 
 let playground = document.getElementById("playground");
+let container_button_start = document.getElementById("button_start_container");
 let button_start = document.getElementById("button_start");
+let dot = document.getElementById("dot");
 let reading_alpha = document.getElementById("reading_alpha");
 let reading_beta = document.getElementById("reading_beta");
 let reading_gamma = document.getElementById("reading_gamma");
-reading_alpha.innerHTML = 0;
-reading_beta.innerHTML = 0;
-reading_gamma.innerHTML = 0;
+let position_x = 0;
+let position_y = 0;
+let velocity_x = 0.0;
+let velocity_y = 0.0;
+let rate_update = 1/6;
 
 function initiate() {
-  let drawing = playground.getContext("2d");
-  drawing.fillStyle = "#FFFFFF";
-  drawing.fillRect(0, 0, 5, 5);
-  console.log("width: " + playground.width + " & height:" + playground.height);
+  dot.style.transform = "translate(" + (playground.offsetWidth / 2) + "px, " + (playground.offsetHeight / 2) + "px";
   button_start.addEventListener("click", startGame, false);
 }
 
 function startGame() {
+  container_button_start.style.display = "none";
   DeviceMotionEvent.requestPermission()
     .then(response => {
       if (response == 'granted') {
         window.addEventListener('deviceorientation', (event) => {
-          reading_alpha.innerHTML = event.alpha;
-          reading_beta.innerHTML = event.beta;
-          reading_gamma.innerHTML = event.gamma;
+          // reading_alpha.innerHTML = event.alpha;
+          // reading_beta.innerHTML = event.beta;
+          // reading_gamma.innerHTML = event.gamma;
+          velocity_x += velocity_x + event.gamma + ( rate_update * 2 );
+          velocity_y += velocity_y + event.beta;
+          position_x += velocity_x;
+          position_y += velocity_y;
+          dot.style.left = position_x + "px";
+          dot.style.top = position_y + "px";
         });
       }
     })
