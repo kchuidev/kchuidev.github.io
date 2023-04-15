26f8e199d52671406fda98fc75664ac805125bc6
kchuidev <121410967+kchuidev@users.noreply.github.com>
2023-04-15 08:52:21 +0800

    Add furigana to Japanese Vocabulator

--- assets/scripts/japanese-vocabulator.js
+++ assets/scripts/japanese-vocabulator.js
@@ -8,16 +8,30 @@ let entry_registered;
 let entry_selected;
 let entries_registered;
 let entries_stored = JSON.parse(localStorage.getItem("japanese-vocabulator_entries_registered"));
-let words_stored = [];
 let entries_stored_loaded = false;
+let words_stored = [];
+let types = {
+  "名": "名詞",
+  "サ": "サ変動詞",
+  "カ": "カ変動詞",
+  "一": "一段動詞",
+  "五": "五段動詞",
+  "ナ": "ナ形容詞",
+  "イ": "イ形容詞",
+  "副": "副詞",
+  "四": "四字熟語",
+  "諺": "諺"
+  
+};
 let description = document.getElementById("description");
 let details = document.getElementById("details");
 let list = document.getElementById("list");
 let display_word = document.getElementById("display_word");
+let display_furigana = document.getElementById("display_furigana");
 let display_type = document.getElementById("display_type");
 let display_meaning = document.getElementById("display_meaning");
-let removal_signs = document.querySelectorAll(".removal_signs");
 let input_word = document.getElementById("input_word");
+let input_furigana = document.getElementById("input_furigana");
 let input_type = document.getElementById("input_type");
 let input_meaning = document.getElementById("input_meaning");
 let list_entry = document.getElementById("entry_list");
@@ -48,6 +62,7 @@ function initiate() {
 function resetInput() {
   entry_registered = {};
   input_word.value = null;
+  input_furigana.value = null;
   input_type.value = "";
   input_meaning.value = null;
   return;
@@ -63,7 +78,7 @@ function validateInput() {
       input_word.focus();
       document.querySelector("label[for='" + input_word.id + "']").classList.add("error");
       return false;
-    case ( !/[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/.test(input_word.value) ):
+    case ( !/[\u3040-\u309f\u30a0-\u30ff\u4e00-\u9faf\u3400-\u4dbf]/.test(input_word.value) ):
       alert("単語のフィールドに日本語だけ入力してください。");
       input_word.focus();
       document.querySelector("label[for='" + input_word.id + "']").classList.add("error");
@@ -77,6 +92,16 @@ function validateInput() {
       alert("同じな単語はもう登録しました。");
       input_word.focus();
       document.querySelector("label[for='" + input_word.id + "']").classList.add("error");
+      return false;
+    case ( input_furigana.value == null || input_furigana.value == "" ):
+      alert("振り仮名を入力してください。");
+      input_furigana.focus();
+      document.querySelector("label[for='" + input_furigana.id + "']").classList.add("error");
+      return false;
+    case ( !/[\u3040-\u309f\u30a0-\u30ff]/.test(input_furigana.value) ):
+      alert("振り仮名のフィールドに仮名だけ入力してください。");
+      input_furigana.focus();
+      document.querySelector("label[for='" + input_furigana.id + "']").classList.add("error");
       return false;
     case ( input_type.value == null || input_type.value == "" ):
       alert("品詞を選んでください。");
@@ -100,6 +125,7 @@ function registerEntry() {
     list.classList.remove("hidden");
     entry_registered = {};
     entry_registered.word = input_word.value;
+    entry_registered.furigana = input_furigana.value;
     entry_registered.type = input_type.value;
     entry_registered.meaning = input_meaning.value;
     displayEntry(entry_registered);
@@ -123,8 +149,9 @@ function displayEntry(object_entry) {
   let entry_added = document.createElement("div");
   entry_added.classList.add("entries");
   entry_added.innerText = object_entry.word;
-  entry_added.innerText += "（" + object_entry.type[0] + "）";
+  entry_added.innerText += "（" + object_entry.type + "）";
   entry_added.dataset.word = object_entry.word;
+  entry_added.dataset.furigana = object_entry.furigana;
   entry_added.dataset.type = object_entry.type;
   entry_added.dataset.meaning = object_entry.meaning;
   col_added.appendChild(entry_added);
@@ -137,6 +164,7 @@ function clearDetails() {
   entry_selected = "";
   button_remove.disabled = true;
   display_word.innerHTML =  "-";
+  display_furigana.innerHTML =  "-";
   display_type.innerHTML =  "-";
   display_meaning.innerHTML =  "-";
   return;
@@ -147,13 +175,13 @@ function displayDetails(event) {
   entry_selected = entry_targeted.dataset.word;
   button_remove.disabled = false;
   display_word.innerHTML =  entry_targeted.dataset.word;
-  display_type.innerHTML =  entry_targeted.dataset.type;
+  display_furigana.innerHTML =  entry_targeted.dataset.furigana;
+  display_type.innerHTML =  types[entry_targeted.dataset.type];
   display_meaning.innerHTML =  entry_targeted.dataset.meaning;
   return;
 }
 
 function removeEntry() {
-  console.log(entries_registered);
   if ( entries_registered != undefined && entry_selected != "" && !button_remove.disabled ) {
     entries_registered.splice( entries_registered.findIndex((e) => e.word === entry_selected), 1 );
     localStorage.setItem("japanese-vocabulator_entries_registered", JSON.stringify(entries_registered));
