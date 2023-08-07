d9c893bd2812da72be72f974d20e1c75af6728d3
kchuidev <121410967+kchuidev@users.noreply.github.com>
2023-08-07 10:53:49 +0800

    Update Japanese Vocabulator instructions

--- assets/scripts/japanese-vocabulator.js
+++ assets/scripts/japanese-vocabulator.js
@@ -330,13 +330,13 @@ function showInstructions() {
   let button_back = document.getElementById("back_button");
   let button_dismiss = document.getElementById("dismiss_button");
   let instructions_introduction = "<img src='assets/images/irasutoya/ojigi_animal_inu.png' class='instructions_images max-width_40percent'>日本語ボキャブレーターのご利用ありがとうございます。当アプリは、語彙力を高めることを目指している日本語学習者向けのアプリです。当アプリを使って、単語をためたり、漢字の書き方を理解したりできます。これから、当アプリの使い方を紹介させていただきます。";
-  let instructions_input = "<img src='assets/images/japanese-vocabulator/instructions_input.png' class='instructions_images'>登録したい単語、その単語の振り仮名、品詞と意味をそれぞれのフィールドに入力して、登録ボタンをクリックしてください。";
+  let instructions_input = "<img src='assets/images/japanese-vocabulator/instructions_input.png' class='instructions_images'>登録したい単語、その単語の振り仮名、品詞と意味をそれぞれのフィールドに入力して、登録ボタンを押してください。";
   let instructions_input_error = "<img src='assets/images/japanese-vocabulator/instructions_input_error.png' class='instructions_images'>入力した情報が不具合なら、エラーメッセージが出て、そのフィールドは赤になります。その時、エラーメッセージの指示を従って情報を再入力してください。";
-  let instructions_list = "<img src='assets/images/japanese-vocabulator/instructions_list.png' class='instructions_images'>単語を登録してから、単語リストが生成します。リストの単語をクリックすると、その単語の情報と書き順が現れます。削除のボタンをクリックして、その単語をリストから取り去れます。単語リストが今使っているブラウザーにたまっていますので、他のブラウザーでそのリストが見ることができません。単語リストを他のブラウザーに転送したいとき、エクスポート機能とインポート機能を使ってください。";
-  let instructions_stroke_order = "<img src='assets/images/japanese-vocabulator/instructions_stroke_order.png' class='instructions_images'>書き順の動画が終わったら、漢字や仮名をクリックすると、その漢字や仮名の動画が再生します。";
+  let instructions_list = "<img src='assets/images/japanese-vocabulator/instructions_list.png' class='instructions_images'>単語を登録してから、単語リストが生成します。リストの単語を選ぶと、その単語の情報と書き順が現れます。削除のボタンを押して、その単語をリストから取り去れます。単語リストが今使っているブラウザーにたまっていますので、他のブラウザーでそのリストが見ることができません。単語リストを他のブラウザーに転送したいとき、エクスポート機能とインポート機能を使ってください。";
+  let instructions_stroke_order = "<img src='assets/images/japanese-vocabulator/instructions_stroke_order.png' class='instructions_images'>書き順の動画が終わったら、漢字や仮名を押すと、その漢字や仮名の動画が再生します。";
   let instructions_filter = "<img src='assets/images/japanese-vocabulator/instructions_filter.png' class='instructions_images'>単語リストのフィルターをかけって、特定の品詞に属する単語を表したり、隠したりできます。";
-  let instructions_export = "<img src='assets/images/japanese-vocabulator/instructions_export.png' class='instructions_images'>エクスポートのボタンをクリックすると、全ての単語についての情報を含むcsvファイルがダウンロードできます。単語リストが空いている場合には、エクスポート機能が使用できません。バックアップのために、定期的に単語をエクスポートすることが勧められます。";
-  let instructions_import = "<img src='assets/images/japanese-vocabulator/instructions_import.png' class='instructions_images'>インポートのボタンをクリックして、前にエクスポートしたcsvファイルを選ぶと、そのcsvファイルの単語がリストに加えられます。エクスポートしたcsvファイルが改竄されれば、エクスポート機能が使用できない可能性があります。";
+  let instructions_export = "<img src='assets/images/japanese-vocabulator/instructions_export.png' class='instructions_images'>エクスポートのボタンを押すと、全ての単語についての情報を含むcsvファイルがダウンロードできます。単語リストが空いている場合には、エクスポート機能が使用できません。バックアップのために、定期的に単語をエクスポートすることが勧められます。";
+  let instructions_import = "<img src='assets/images/japanese-vocabulator/instructions_import.png' class='instructions_images'>インポートのボタンを押して、前にエクスポートしたcsvファイルを選ぶと、そのcsvファイルの単語がリストに加えられます。エクスポートしたcsvファイルが改竄されれば、エクスポート機能が使用できない可能性があります。";
   let instructions_end = "<img src='assets/images/irasutoya/ojigi_animal_inu.png' class='instructions_images max-width_40percent'>使い方ガイドはここまでです。当アプリが皆さんの力になれば嬉しいです。重ねてご利用ありがとうございます。";
   let instructions_order = [instructions_introduction, instructions_input, instructions_input_error, instructions_list, instructions_stroke_order, instructions_filter, instructions_export, instructions_import, instructions_end];
 
